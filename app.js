9344229b7a9d5ea6dadc2f118ca13576e43b6468
juanospina202032 <123768364+juanospina202032@users.noreply.github.com>
2023-02-10 13:59:40 -0500

    quiz, imgs

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 document.addEventListener("DOMcontentLoaded"), () => {});
+  const cardadj = [
+    {
+      name: "1",
+      img: "images/1.jpg"
+    },
+    {
+      name: "2",
+      img: "images/2.jpg"
+    },
+    {
+      name: "3",
+      img: "images/3.jpg"
+    },
+    {
+      name: "4",  
+      img: "images/4.jpg"
+    },
+    {
+      name: "5",
+      img: "images/5.jpg"
+    },
+    { 
+      name: "6",
+      img: "images/6.jpg"
+    },
+    {
+      name: "7",
+      img: "images/7.jpg"
+    },
+    {
+      name "8",
+      img   
+
